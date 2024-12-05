20bc21a76b9cb63782eb573c03faafec7b5add91
Jessica Xu <jjx@mit.edu>
2024-12-05 13:57:34 -0500

    store assistant response in token map

--- examples/run_streaming_llama_index.py
+++ examples/run_streaming_llama_index.py
@@ -20,7 +20,7 @@ from transformers import (
 )
 
 @torch.no_grad()
-def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len):
+def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len, kv_cache=None):
     outputs = model(
         input_ids=input_ids,
         past_key_values=past_key_values,
@@ -30,6 +30,7 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len):
     pred_token_idx = outputs.logits[:, -1, :].argmax(dim=-1).unsqueeze(1)
     generated_ids = [pred_token_idx.item()]
     pos = 0
+    generated_response = ""
     for _ in range(max_gen_len - 1):
         outputs = model(
             input_ids=pred_token_idx,
@@ -53,16 +54,26 @@ def greedy_generate(model, tokenizer, input_ids, past_key_values, max_gen_len):
         now = len(generated_text) - 1
         if now > pos:
             print(" ".join(generated_text[pos:now]), end=" ", flush=True)
+            generated_response += " ".join(generated_text[pos:now]) + " "
             pos = now
+
 
         if pred_token_idx == tokenizer.eos_token_id:
             break
     print(" ".join(generated_text[pos:]), flush=True)
+    generated_response += " ".join(generated_text[pos:])
+
+    if kv_cache is not None:
+        response_ids = tokenizer(generated_response, return_tensors="pt").input_ids
+        response_ids = response_ids.to(model.device)
+        response_tokens = tokenizer.convert_ids_to_tokens(response_ids[0])    
+        kv_cache.store_tokens(tokens=response_tokens)
+    
     return past_key_values
 
 
 @torch.no_grad()
-def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=1000):
+def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=500):
     past_key_values = None
     for idx, prompt in enumerate(prompts):
         prompt = "USER: " + prompt + "\n\nASSISTANT: "
@@ -79,11 +90,10 @@ def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=10
             kv_cache.store_tokens(tokens=tokens)
 
             # Get past key values and evict for space
-            if idx < 2:
+            if idx != 1:
                 space_needed = seq_len + max_gen_len
                 past_key_values = kv_cache.evict_for_space(past_key_values, space_needed)
-            elif idx == 2: 
-                # TODO: Retrieve relevant past context for all prompts (not just for testing).
+            elif idx == 1: 
                 # For testing, try retrieving relevant past context for the second prompt
                 print("Retrieving relevant context")
 
@@ -102,12 +112,12 @@ def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=10
                 # Get past key values with past context included in cache
                 space_needed = seq_len + max_gen_len + past_seq_len
                 past_key_values = kv_cache.evict_for_space(past_key_values, space_needed, past_context=past_context_string)
-            else:
-                # For testing, it stops after the second prompt
-                raise ValueError(f"stop here")
+            # else:
+            #     # For testing, it stops after the second prompt
+            #     raise ValueError(f"stop here")
 
         past_key_values = greedy_generate(
-            model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
+            model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len, kv_cache=kv_cache
         )
 
 
@@ -170,7 +180,8 @@ def main(args):
 
     model.eval()
 
-    test_filepath = os.path.join("examples", "data", "secret_words.jsonl")
+    # test_filepath = os.path.join("examples", "data", "secret_words.jsonl")
+    test_filepath = os.path.join(args.data_root, "mt_bench.jsonl")
     
     print(f"Loading data from {test_filepath} ...")
 
@@ -211,9 +222,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_root", type=str, default="data/")
     parser.add_argument("--enable_streaming", action="store_true")
     parser.add_argument("--start_size", type=int, default=4)
-    parser.add_argument("--recent_size", type=int, default=128)
+    parser.add_argument("--recent_size", type=int, default=1000)
     args = parser.parse_args()
 
     main(args)
-
-
